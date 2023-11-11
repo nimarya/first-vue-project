01626a77adcbeeb5a542eca7f00be3c276aa83f8
nimarya <masha.nichiporenko@gmail.com>
2023-11-11 14:17:22 +0300

    made text in input save in localStorage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,24 @@
 // import TheWelcome from '../components/TheWelcome.vue'
 import { ref } from 'vue';
 
-let name = ref();
-let hometown = ref();
+let name = ref(localStorage.getItem('name'));
+let hometown = ref(localStorage.getItem('hometown'));
+
+function write(key, value) {
+  localStorage.setItem(key, value);
+}
 
 </script>
 
 <template>
   <main>
     <p>
-      What is your name? <input type="text" v-model="name">
+      What is your name? <input type="text" v-model="name" @input="write('name', name)">
       <p>My name is {{ name ? name : 'Maria' }}.</p>
     </p>
 
     <p>
-      What is your hometown? <input type="text" v-model="hometown">
+      What is your hometown? <input type="text" v-model="hometown" @input="write('hometown', hometown)">
       <p>I am from {{ hometown ? hometown : 'Moscow' }}.</p>
     </p>
   </main>
