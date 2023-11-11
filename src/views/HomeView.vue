8049b1bb671e1ef2fab1b61a70b3fad012040773
nimarya <masha.nichiporenko@gmail.com>
2023-11-11 14:04:19 +0300

    exapmle of simple form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+
+let name = ref();
+let hometown = ref();
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>
+      What is your name? <input type="text" v-model="name">
+      <p>My name is {{ name ? name : 'Maria' }}.</p>
+    </p>
+
+    <p>
+      What is your hometown? <input type="text" v-model="hometown">
+      <p>I am from {{ hometown ? hometown : 'Moscow' }}.</p>
+    </p>
   </main>
 </template>
