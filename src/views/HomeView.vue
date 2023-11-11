56bc0ab04d7835c524075bd7545b8d26ac713245
nimarya <masha.nichiporenko@gmail.com>
2023-11-11 15:31:12 +0300

    another way to trach the changes of the variable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 let name = ref(localStorage.getItem('name'));
 let hometown = ref(localStorage.getItem('hometown'));
+
+watch(name, (value) => {
+  write('name', value);
+});
+
+watch(hometown, (value) => {
+  write('hometown', value);
+});
 
 function write(key, value) {
   localStorage.setItem(key, value);
@@ -14,12 +22,12 @@ function write(key, value) {
 <template>
   <main>
     <p>
-      What is your name? <input type="text" v-model="name" @input="write('name', name)">
+      What is your name? <input type="text" v-model="name">
       <p>My name is {{ name ? name : 'Maria' }}.</p>
     </p>
 
     <p>
-      What is your hometown? <input type="text" v-model="hometown" @input="write('hometown', hometown)">
+      What is your hometown? <input type="text" v-model="hometown">
       <p>I am from {{ hometown ? hometown : 'Moscow' }}.</p>
     </p>
   </main>
